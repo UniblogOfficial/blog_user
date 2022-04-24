42d6a3b78134a7d32ab940983dc64bda5eefe6d5
Alex <alexeyvasin203@gmail.com>
2022-04-24 22:34:12 +0300

    [add] refactor

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from app.api import app
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5015)
+    app.run(debug=True, host='0.0.0.0', port=5025)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
