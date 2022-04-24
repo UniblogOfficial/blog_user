01b2c9b12905017bc84998b8eff8bd82e224bab1
Alex <alexeyvasin203@gmail.com>
2022-04-24 22:39:18 +0300

    [add]  bugfix

--- app/api.py
+++ app/api.py
@@ -4,14 +4,16 @@ from flask import Flask, request, Response, jsonify, abort
 from email_validator import validate_email, EmailNotValidError
 from flask_jwt_extended import JWTManager, create_access_token, \
     jwt_required, get_jwt_identity
+from flask_restx import Api, Resource, fields
 from app.database.db import initialize_db
 from app.database.models import User, Social
-from flasgger import Swagger
 from datetime import datetime, timedelta
+
 import imghdr
 import string
 
 import bcrypt
+import tempfile
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
@@ -22,9 +24,8 @@ initialize_db(app)
 app.config['SECRET_KEY'] = 'super-secret'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
 
-swagger = Swagger(app)
-
 jwt = JWTManager(app)
+api = Api(app)
 
 
 def validate_data(data: dict) -> ():
@@ -43,80 +44,107 @@ def validate_data(data: dict) -> ():
     return 1, 'ok'
 
 
-@app.route('/create_user', methods=['POST', 'GET'])
-def create_user():
-    data = request.get_json()
-    if 'email' not in data and \
-            'password' not in data:
-        return Response(status=401)
-    state, messange = validate_data(data)
-    if not state:
-        return {"code": "401", "state": messange}
-    data['password'] = bcrypt.hashpw(data['password'].encode(), bcrypt.gensalt())
-    # try:
-    user = User(**data).save()
-    return jsonify(user), 201
+@api.route('/create_user', endpoint='create_user')
+@api.doc(params={'email': 'User email',
+                 'password': 'User password'})
+class CreateUser(Resource):
+    @staticmethod
+    def post():
+        data = request.get_json()
+        if 'email' not in data and \
+                'password' not in data:
+            return Response(status=401)
+        state, messange = validate_data(data)
+        if not state:
+            return {"code": "401", "state": messange}
+        data['password'] = bcrypt.hashpw(data['password'].encode(),
+                                         bcrypt.gensalt())
+        user = User(**data).save()
+        return jsonify(user), 201
 
 
-@app.route('/login', methods=['POST'])
-def loging():
-    if not request.get_json().get('email') and not \
-            request.get_json().get('password'):
-        return Response('', status=401)
-    data = request.get_json()
-    user = User.objects(email=data['email']).first()
-    if bcrypt.checkpw(data['password'].encode('utf-8'),
+@api.route('/logging', endpoint='logging')
+@api.doc(params={'email': 'User email',
+                 'password': 'User password'})
+class Logging(Resource):
+    @staticmethod
+    def post():
+        if not request.get_json().get('email') and not \
+                request.get_json().get('password'):
+            return Response('', status=401)
+        data = request.get_json()
+        user = User.objects(email=data['email']).first()
+        if bcrypt.checkpw(data['password'].encode('utf-8'),
                       user.password.encode('utf-8')):
-        dt = datetime.now()
-        token = create_access_token(
-            identity=f"{random.choice(string.ascii_letters)}"
+            dt = datetime.now()
+            token = create_access_token(
+                identity=f"{random.choice(string.ascii_letters)}"
                      f"{data['email']}{dt.microsecond}",
-            fresh=timedelta(minutes=9999))
-        return {"token": token, "id": str(user["id"])}
-    return Response('error', status=401)
+                fresh=timedelta(minutes=9999))
+            return {"token": token, "id": str(user["id"])}
+        return Response('error', status=401)
 
 
-@app.route('/user/<id_>', methods=['GET', 'PUT', 'DELETE'])
-@jwt_required()
-def user(id_):
-    get_jwt_identity()
-    user = User.objects(id=id_).first()
-    if not user:
-        return {"data": "Пользователь не найден"},
-    return {"data": user}
+@api.route('/user/<id_>', endpoint='user-id')
+@api.doc()
+class UserId(Resource):
+    @jwt_required()
+    def get(self, id_):
+        get_jwt_identity()
+        user = User.objects(id=id_).first()
+        if not user:
+            return {"data": "Пользователь не найден"},
+        return jsonify(user)
+
+    @staticmethod
+    def put():
+        pass
+
+    @staticmethod
+    def delete():
+        pass
 
 
-@app.route('/users', methods=['GET'])
-@jwt_required()
-def users():
-    get_jwt_identity()
-    users = User.objects()
-    return jsonify(users)
+@api.route('/users', endpoint='users')
+@api.doc()
+class Users(Resource):
+    @jwt_required()
+    def get(self):
+        get_jwt_identity()
+        users = User.objects()
+        return jsonify(users)
 
 
-@app.route('/user/<id_>', methods=['PUT'])
-@jwt_required()
-def add_profile_picture(id_):
-    get_jwt_identity()
-    user = User.objects(id=id_).first()
-    if request.files['file']:
-        image = request.files['file']
-        # if imghdr.what(image.filename) in ('jpeg', 'png', 'gif', 'tiff'):
-        user.profile_image.put(image, filename=image.filename)
-    else:
-        user.update_one(**request.data())
-    user.save()
-    return jsonify(user), 201
-    # else:
-    #    return jsonify({"data": "image error"}), 400
+@api.route('/user/<id_>/image', endpoint='user-image')
+@api.doc(params={'id': 'user id',
+                 'file': 'file image'})
+class UserPicture(Resource):
+    @staticmethod
+    @jwt_required()
+    def put(id_):
+        get_jwt_identity()
+        user = User.objects(id=id_).first()
+        if request.files['file']:
+            temp = tempfile.TemporaryDirectory()
+            image = request.files['file']
+            image.save(f"{image.filename}")
+            if not imghdr.what(f"{image.filename}") in ('jpeg', 'png', 'gif', 'tiff'):
+                return {"data": "image error"}, 400
+        else:
+            user.update_one(**request.data())
+        user.save()
+        return jsonify(user)
 
 
-@app.route('/user/<id_>/user_social', methods=['POST'])
-@jwt_required()
-def add_social(id_):
-    get_jwt_identity()
-    data = request.get_json()
-    user = User.objects(id=id_).first()
-    social = Social(**data).save()
-    user.social.append(social)
-    User().save()
+@api.route('/user/<id_>/user_social', endpoint='user-social')
+@api.doc(params={'id': 'User id',
+                 'data_social': '?'})
+class Social(Resource):
+    @jwt_required()
+    def post(self, id_):
+        get_jwt_identity()
+        data = request.get_json()
+        user = User.objects(id=id_).first()
+        social = Social(**data).save()
+        user.social.append(social)
+        User().save()
--- app/database/models.py
+++ app/database/models.py
@@ -12,7 +12,7 @@ class Social(db.Document):
 class User(db.Document):
     profile_image = db.FileField()
     name = db.StringField(unique=True)
-    password = db.StringField(required=True)
+    password = db.StringField(required=True, selected=False)
     email = db.EmailField(unique=True, required=True)
     social = db.ListField(db.ReferenceField(Social))
 
